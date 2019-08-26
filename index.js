ebc98bfa64f0049a9c4cc82127646a2e58d1feb4
Valentino Wong <valentino.wong@gmail.com>
2019-08-25 23:25:27 -0400

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCase(str) {
+  return str.toLowerCase().split(' ').map(function(word) {
+    return (word.charAt(0).toUpperCase() + word.slice(1));
+  }).join(' ');
 }
+
+const titleCased = tutorials.map(tutorial => titleCase(tutorial))
+
+
+// const titleCased = () => {
+//   return tutorials
+// }
