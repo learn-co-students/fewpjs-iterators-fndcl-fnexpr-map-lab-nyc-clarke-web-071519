9267f238c7fbb1808ae91cb622e739a03fd40bcf
Valentino Wong <valentino.wong@gmail.com>
2019-08-25 23:31:14 -0400

    Done.

--- index.js
+++ index.js
@@ -12,12 +12,14 @@ const tutorials = [
 ];
 
 function titleCase(str) {
-  return str.toLowerCase().split(' ').map(function(word) {
+  return str.split(' ').map(function(word) {
     return (word.charAt(0).toUpperCase() + word.slice(1));
   }).join(' ');
 }
 
-const titleCased = tutorials.map(tutorial => titleCase(tutorial))
+function titleCased() { 
+  return tutorials.map(tutorial => titleCase(tutorial));
+}
 
 
 // const titleCased = () => {
